d42aa1a1aa69060c77a5e2ea61aeb130ad5fde57
nigel <nmillward@tripadvisor.com>
2015-09-21 21:19:57 +0100

    hot or cold

---
+++ js/app.js
@@ -0,0 +1,115 @@
+
+$(document).ready(function(){
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+	// Generate random number
+ 	function randomNumber()
+	{
+		return  Math.floor((Math.random() * 100) + 1); 
+	}
+
+	var rand = randomNumber();
+
+	// Count guess 
+	var guessNumber = 0;
+ 	function guessCount() 
+ 	{
+    	guessNumber += 1;  
+    	$("#count").text(guessNumber);
+
+ 	}
+
+ 	function difference(userGuess, rand)
+ 	{
+  		if(userGuess > rand)
+  		{
+  			return userGuess-rand;
+  		} 
+  		else 
+  		{
+  			return rand-userGuess;
+  		}
+	}
+
+	// Response to guess
+    function submitAnswer(diff)
+    {
+		
+		if(diff == 0)
+		{
+			$('#feedback').text("you are correct");
+		}
+		if(diff > 0 && diff <= 10)
+		{
+			$('#feedback').text("You are very hot!");
+		}
+		if(diff => 11 && diff <= 20)
+		{
+			$('#feedback').text("You are hot!");
+		}
+		if(diff => 21 && diff <= 30)
+		{
+			$('#feedback').text("You are warm");
+		}
+		if(diff => 31 && diff <= 50)
+		{
+			$('#feedback').text("You are cold");
+		}
+		if(diff > 50)
+		{
+			$('#feedback').text("You are ice cold");
+		}
+	}
+
+	// Click the 'guess' button
+	$('#guessButton').on('click', function()
+	{
+		var userGuess = $('#userGuess').val();
+		var diff = difference(userGuess, rand);
+
+		if(userGuess % 1 == 0 && userGuess <= 100 && userGuess > 0)
+		{
+			$('.guessBox').append('<li>' + userGuess + '</li>');
+			guessCount();
+			submitAnswer(diff);
+			
+			console.log('userGuess: ' + userGuess);
+			console.log('random number: ' + rand);
+			console.log('diff: ' + diff);
+			console.log('submit answer: ' + submitAnswer(diff));
+			$('#userGuess').val("");
+		} else {
+			alert('Type only numbers between 1 and 100.');
+      		$('#userGuess').val("");
+		}
+	});
+
+	// Click on the 'new game' button
+	$('.new').on('click', function()
+	{
+		location.reload();
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// new game function to start a new game
